dd0b97973c55d8d3b24964740d1150a53904f5cf
reus <reusee@ymail.com>
2013-07-07 20:08:25 +0800

    server: refactoring

--- server.go
+++ server.go
@@ -43,6 +43,7 @@ type Session struct {
   session *session.Session
   sendQueue *ic.InfiniteChan
   targetConn *net.TCPConn
+  hostPort string
 }
 
 func handleClient(conn *net.TCPConn) {
@@ -50,37 +51,43 @@ func handleClient(conn *net.TCPConn) {
 
   targetReader := cr.New()
   comm := session.NewComm(conn)
+  targetConnEvents := ic.New()
+  connectTarget := func(session *Session) {
+    defer func() {
+      targetConnEvents.In <- session
+    }()
+    addr, err := net.ResolveTCPAddr("tcp", session.hostPort)
+    if err != nil { return }
+    targetConn, err := net.DialTCP("tcp", nil, addr)
+    if err != nil { return }
+    targetReader.Add(targetConn, session)
+    session.targetConn = targetConn
+    return
+  }
 
   loop: for { select {
-  // messages from local 
-  case msg := <-comm.Messages:
-    switch msg.Type {
+  // local-side events
+  case evI := <-comm.Events.Out:
+    ev := evI.(session.Event)
+    switch ev.Type {
     case session.SESSION: // new local session
       session := &Session{
         sendQueue: ic.New(),
+        hostPort: string(ev.Data),
       }
-      session.session = comm.NewSession(msg.Session.Id, nil, session)
-      go func() {
-        addr, err := net.ResolveTCPAddr("tcp", string(msg.Data))
-        hostError := false
-        if err != nil { hostError = true }
-        targetConn, err := net.DialTCP("tcp", nil, addr)
-        if err != nil { hostError = true }
-        if !hostError {
-          targetReader.Add(targetConn, session)
-          session.targetConn = targetConn
-        }
-        for {
-          data := (<-session.sendQueue.Out).([]byte)
-          if !hostError { targetConn.Write(data) }
-        }
-      }()
+      session.session = comm.NewSession(ev.Session.Id, nil, session)
+      go connectTarget(session)
     case session.DATA: // local data
-      msg.Session.Obj.(*Session).sendQueue.In <- msg.Data
+      session := ev.Session.Obj.(*Session)
+      if session.targetConn == nil {
+        session.sendQueue.In <- ev.Data
+      } else {
+        session.targetConn.Write(ev.Data)
+      }
     case session.CLOSE: // local session closed
-      msg.Session.Close()
+      ev.Session.Close()
       defer func() {
-        targetConn := msg.Session.Obj.(*Session).targetConn
+        targetConn := ev.Session.Obj.(*Session).targetConn
         if targetConn != nil {
           <-time.After(time.Second * 5)
           targetConn.Close()
@@ -89,13 +96,22 @@ func handleClient(conn *net.TCPConn) {
     case session.ERROR: // error
       break loop
     }
-  // messages from target host
-  case msgI := <-targetReader.Messages.Out:
-    msg := msgI.(cr.Message)
-    session := msg.Obj.(*Session)
-    switch msg.Type {
+  // target connection events
+  case sessionI := <-targetConnEvents.Out:
+    session := sessionI.(*Session)
+    readQueue: for { select {
+    case dataI := <-session.sendQueue.Out:
+      data := dataI.([]byte)
+      session.targetConn.Write(data)
+    default: break readQueue
+    }}
+  // target events
+  case evI := <-targetReader.Events.Out:
+    ev := evI.(cr.Event)
+    session := ev.Obj.(*Session)
+    switch ev.Type {
     case cr.DATA:
-      session.session.Send(msg.Data)
+      session.session.Send(ev.Data)
     case cr.EOF, cr.ERROR:
       session.session.Close()
       session.sendQueue.Close()
