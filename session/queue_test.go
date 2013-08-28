3d86f0793c3703945ab8c0524731eb6200fca1b3
reus <reusee@ymail.com>
2013-08-28 18:55:02 +0800

    Revert "session: remove unused code"
    This reverts commit 65f3761324830b3a0f659e740171f20df3001f64.

---
+++ session/queue_test.go
@@ -0,0 +1,57 @@
+package session
+
+import (
+	"testing"
+)
+
+func TestQueue(t *testing.T) {
+	q := NewQueue()
+	q.En(&Packet{serial: 1})
+	q.En(&Packet{serial: 2})
+	q.En(&Packet{serial: 3})
+	q.En(&Packet{serial: 4})
+	p := q.De()
+	if p.serial != 1 {
+		t.Fail()
+	}
+	p = q.De()
+	if p.serial != 2 {
+		t.Fail()
+	}
+	p = q.De()
+	if p.serial != 3 {
+		t.Fail()
+	}
+	p = q.De()
+	if p.serial != 4 {
+		t.Fail()
+	}
+	p = q.De()
+	if p != nil {
+		t.Fail()
+	}
+	q.En(&Packet{serial: 1})
+	q.En(&Packet{serial: 2})
+	q.En(&Packet{serial: 3})
+	q.En(&Packet{serial: 4})
+	p = q.De()
+	if p.serial != 1 {
+		t.Fail()
+	}
+	p = q.De()
+	if p.serial != 2 {
+		t.Fail()
+	}
+	p = q.De()
+	if p.serial != 3 {
+		t.Fail()
+	}
+	p = q.De()
+	if p.serial != 4 {
+		t.Fail()
+	}
+	p = q.De()
+	if p != nil {
+		t.Fail()
+	}
+}
